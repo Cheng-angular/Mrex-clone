31f0ac4672649809e2360080297cfff23114ec5a
full-stack-angular <angulardev0811@outlook.com>
2020-11-25 13:28:43 -0300

    router-change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,17 +11,17 @@ import { TermConditionComponent } from './pages/term-condition/term-condition.co
 const routes: Routes = [
   // { path: '', redirectTo: '', pathMatch: 'full'},
   { path: '', loadChildren: () => import('./pages/landingpage/landingpage.module').then( m => m.LandingpageModule), pathMatch: 'full'},
-  { path: 'product-terminal', loadChildren: () => import('./pages/product-terminal/product-terminal.module').then( m => m.ProductTerminalModule)},
-  { path: 'product-tradelab', loadChildren: () => import('./pages/product-tradelab/product-tradelab.module').then( m => m.ProductTradelabModule)},
-  { path: 'product-message', loadChildren: () => import('./pages/product-message/product-message.module').then( m => m.ProductMessageModule)},
-  { path: 'about-company', loadChildren: () => import('./pages/about-company/about-company.module').then( m => m.AboutCompanyModule)},
-  { path: 'about-career', loadChildren: () => import('./pages/about-careers/about-careers.module').then( m => m.AboutCareersModule)},
-  { path: 'about-investor', loadChildren: () => import('./pages/about-investor/about-investor.module').then( m => m.AboutInvestorModule)},
+  { path: 'product-terminal', loadChildren: () => import('./pages/product-terminal/product-terminal.module').then( m => m.ProductTerminalModule), pathMatch: 'full'},
+  { path: 'product-tradelab', loadChildren: () => import('./pages/product-tradelab/product-tradelab.module').then( m => m.ProductTradelabModule), pathMatch: 'full'},
+  { path: 'product-message', loadChildren: () => import('./pages/product-message/product-message.module').then( m => m.ProductMessageModule), pathMatch: 'full'},
+  { path: 'about-company', loadChildren: () => import('./pages/about-company/about-company.module').then( m => m.AboutCompanyModule), pathMatch: 'full'},
+  { path: 'about-career', loadChildren: () => import('./pages/about-careers/about-careers.module').then( m => m.AboutCareersModule), pathMatch: 'full'},
+  { path: 'about-investor', loadChildren: () => import('./pages/about-investor/about-investor.module').then( m => m.AboutInvestorModule), pathMatch: 'full'},
   
-  { path: 'research', component: ResearchComponent},
-  { path: 'pricing', component: PricingComponent},
-  { path: 'term-condition', component: TermConditionComponent},
-  { path: 'privacy-policy', component: PrivacyPolicyComponent}
+  { path: 'research', component: ResearchComponent, pathMatch: 'full'},
+  { path: 'pricing', component: PricingComponent, pathMatch: 'full'},
+  { path: 'term-condition', component: TermConditionComponent, pathMatch: 'full'},
+  { path: 'privacy-policy', component: PrivacyPolicyComponent, pathMatch: 'full'}
 
 
 ];
