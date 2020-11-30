b1bcc2e9da88251c4de93508212a6d44c35cf6dd
full-stack-angular <angulardev0811@outlook.com>
2020-11-30 13:07:06 -0300

    change Darqube into mrex

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('daqurbeClone1 app is running!');
+    expect(page.getTitleText()).toEqual('mrex app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'daqurbeClone1'`, () => {
+  it(`should have as title 'mrex'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('daqurbeClone1');
+    expect(app.title).toEqual('mrex');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('daqurbeClone1 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('mrex app is running!');
   });
 });
